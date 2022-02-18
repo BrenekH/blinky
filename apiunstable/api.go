9c8c020707d0d02df141f1a54c6dc9ce9ea5da6a
Brenek Harrison <brenekharrison@gmail.com>
2022-02-18 11:16:20 -0700

    Use gorilla/mux to handle package operations

--- apiunstable/api.go
+++ apiunstable/api.go
@@ -1,9 +1,29 @@
 package apiunstable
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/gorilla/mux"
+)
+
+// Register registers http handlers associated with the unstable API.
 func Register() {
-	http.HandleFunc("/api/unstable/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("pong"))
-	})
+	r := mux.NewRouter()
+	s := r.PathPrefix("/api/unstable/").Subrouter()
+
+	s.HandleFunc("/{repo}/package/{package_name}", handleRepoPkg)
+
+	http.Handle("/", r)
+}
+
+// Routes (all prefixed with /api/unstable):
+// PUT /:repo/package/:package_name
+// DELETE /:repo/package/:package_name
+
+func handleRepoPkg(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	fmt.Println("repo: ", vars["repo"])
+	fmt.Println("package: ", vars["package_name"])
+	w.Write([]byte("Hello"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/BrenekH/blinky
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0
