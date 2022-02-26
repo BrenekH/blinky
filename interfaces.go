b8a5c0fd69668d4dc54bf9456853c867fa7768b6
Brenek Harrison <brenekharrison@gmail.com>
2022-02-26 16:04:26 -0700

    Authenticator interface

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,7 @@
 package blinky
 
+import "net/http"
+
 // The PackageNameToFileProvider interface describes what methods a struct needs
 // to implement in order to be used as a way for Blinky to associate a package
 // to the last file that was uploaded for it.
@@ -7,4 +9,10 @@ type PackageNameToFileProvider interface {
 	PackageFile(packageName string) (filePath string, err error)
 	StorePackageFile(packageName, filePath string) (err error)
 	DeletePackageFileEntry(packageName string) (err error)
+}
+
+// The Authenticator interface describes how a given API expects a struct that
+// provides authentication capabilities to behave.
+type Authenticator interface {
+	CreateMiddleware(http.Handler) http.Handler
 }
