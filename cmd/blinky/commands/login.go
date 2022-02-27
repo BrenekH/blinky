59b59e6fbc3d0a1a8b2cad3f877b3ff165b60b0e
Brenek Harrison <brenekharrison@gmail.com>
2022-02-27 00:50:13 -0700

    Setup flags for login command

--- cmd/blinky/commands/login.go
+++ cmd/blinky/commands/login.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 // loginCmd represents the login command
@@ -12,7 +13,11 @@ var loginCmd = &cobra.Command{
 	Short: "Check and save the login information for a Blinky server.",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("login called")
+		setAsDefault := viper.GetBool("default")
+		flagUsername := viper.Get("username")
+		flagPassword := viper.Get("password")
+
+		fmt.Println(setAsDefault, flagUsername, flagPassword)
 
 		// NOTE: If auth info is already in DB, ask if user would like to update it.
 	},
@@ -20,4 +25,12 @@ var loginCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(loginCmd)
+
+	loginCmd.Flags().BoolP("default", "d", false, "Set server as default for upload and remove")
+	loginCmd.Flags().String("username", "", "Set username for server login")
+	loginCmd.Flags().String("password", "", "Set password for server login. It is recommended to enter the password in the interactive prompt instead of using this flag")
+
+	viper.BindPFlag("default", loginCmd.Flags().Lookup("default"))
+	viper.BindPFlag("username", loginCmd.Flags().Lookup("username"))
+	viper.BindPFlag("password", loginCmd.Flags().Lookup("password"))
 }
