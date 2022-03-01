ebf2fdf6d661343b6c771d1b21716b8f37e77509
Brenek Harrison <brenekharrison@gmail.com>
2022-02-28 21:19:28 -0700

    Add long description for upload command

--- cmd/blinky/commands/upload.go
+++ cmd/blinky/commands/upload.go
@@ -12,9 +12,18 @@ import (
 
 // uploadCmd represents the upload command
 var uploadCmd = &cobra.Command{
-	Use:   "upload repo_name package_file...",
+	Use:   "upload repo_name package_files...",
 	Short: "Upload packages to a Blinky server.",
-	Long:  ``,
+	Long: `Upload multiple packages to a pacman repository hosted
+on a Blinky server. If the --server flag is not provided,
+the default server will be used.
+
+The user may override the saved username and password with the
+--username and --password flags. upload will also prompt for a
+password if --password is not used and --ask-pass is passed.
+
+If a matching ".sig" is found alongside the package file, it will
+be uploaded along with the package to the target server.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		server := viper.GetString("server")
 		username := viper.GetString("username")
