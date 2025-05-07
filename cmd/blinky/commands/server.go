a7f9289f7dcb5431be4ae48dea4533b4ca4dbbab
hayao <shun819.mail@gmail.com>
2025-05-07 13:27:10 +0900

    Enhance server command

--- cmd/blinky/commands/server.go
+++ cmd/blinky/commands/server.go
@@ -8,9 +8,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func printServerInfo(n string, s util.Server) {
+	if s.Username == "" {
+		fmt.Printf("%s\n", n)
+	} else {
+		fmt.Printf("%s(%s)\n", n, s.Username)
+	}
+}
+
 var serverCmd = &cobra.Command{
-	Use:   "server",
+	Use:   "server [server_url...]",
 	Short: "Manage Blinky servers",
+	Long: `Manage Blinky servers. This command is used to list the servers
+that are saved in the server database.
+It can also be used to check the login information for a specific server.`,
+	Args:   cobra.ArbitraryArgs,
+	PreRun: func(cmd *cobra.Command, args []string) {},
 	Run: func(cmd *cobra.Command, args []string) {
 		serverDB, err := util.ReadServerDB()
 		if err != nil {
@@ -18,8 +31,20 @@ var serverCmd = &cobra.Command{
 			os.Exit(1)
 		}
 
-		for name, server := range serverDB.Servers {
-			fmt.Println(name + "(" + server.Username + ")")
+		if len(args) > 0 {
+			for _, server := range args {
+				if _, ok := serverDB.Servers[server]; !ok {
+					fmt.Printf("Server %s not found.\n", server)
+					os.Exit(1)
+				} else {
+					printServerInfo(server, serverDB.Servers[server])
+				}
+			}
+
+		} else {
+			for name, server := range serverDB.Servers {
+				printServerInfo(name, server)
+			}
 		}
 	},
 }
