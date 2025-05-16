dd95230c32e12559e6f3433fe3a95d72de8f96a7
hayao <shun819.mail@gmail.com>
2025-05-16 15:19:54 +0900

    Use gob inside badgerDb

--- keyvaluestore/kv.go
+++ keyvaluestore/kv.go
@@ -19,33 +19,65 @@ type BadgerAdapter struct { // implements: github.com/BrenekH/blinky.PackageName
 	db *badger.DB
 }
 
-func (b *BadgerAdapter) PackageFile(packageName string) (string, error) {
-	// Convert to bytes outside the txn to reduce time spent in txn.
-	key := []byte(packageName)
-
-	var dstBuf []byte
-	err := b.db.View(func(txn *badger.Txn) error {
-		item, err := txn.Get(key)
-		if err != nil {
-			return err
-		}
-
-		dstBuf, err = item.ValueCopy(nil)
-		return err
-	})
+func (b *BadgerAdapter) getTxn(key []byte, txn *badger.Txn) (*pkgInfo, error) {
+	item, err := txn.Get(key)
 	if err != nil {
-		return "", fmt.Errorf("badger.PackageFile view: %w", err)
+		return nil, err
 	}
 
-	return string(dstBuf), nil
+	dstBuf, err := item.ValueCopy(nil)
+	if err != nil || dstBuf == nil {
+		return nil, fmt.Errorf("badger.get view: %w", err)
+	}
+	pi, err := decodePkgInfo(dstBuf)
+	if err != nil {
+		// TODO: How handle legacy data?
+		return nil, fmt.Errorf("badger.PackageFile decode: %w", err)
+	}
+	if pi == nil {
+		return nil, fmt.Errorf("badger.PackageFile decode: nil pkgInfo")
+	}
+
+	return pi, nil
 }
 
-func (b *BadgerAdapter) StorePackageFile(packageName, filePath string) error {
+func (b *BadgerAdapter) get(packageName string) (*pkgInfo, error) {
+	var pi *pkgInfo
+
+	// Convert to bytes outside the txn to reduce time spent in txn.
+	keyBytes := []byte(packageName)
+
+	err := b.db.View(func(txn *badger.Txn) error {
+		var err error
+		pi, err = b.getTxn(keyBytes, txn)
+		return err
+	})
+	return pi, err
+}
+
+func (b *BadgerAdapter) PackageFile(packageName string, packageArch string) (string, error) {
+	pi, err := b.get(packageName)
+	if err != nil {
+		return "", fmt.Errorf("badger.PackageFile get: %w", err)
+	}
+
+	allowKeys := []string{packageArch, "any", ""}
+	for _, k := range allowKeys {
+		if _, ok := pi.files[k]; ok {
+			return pi.files[k], nil
+		}
+	}
+
+	return "", fmt.Errorf("badger.PackageFile: no file found for %s", packageName)
+}
+
+func (b *BadgerAdapter) StorePackageFile(packageName, packageArch, filePath string) error {
 	// Convert to bytes outside the txn to reduce time spent in txn.
 	key := []byte(packageName)
 	val := []byte(filePath)
 
 	err := b.db.Update(func(txn *badger.Txn) error {
+
 		return txn.Set(key, val)
 	})
 	if err != nil {
@@ -55,7 +87,7 @@ func (b *BadgerAdapter) StorePackageFile(packageName, filePath string) error {
 	return nil
 }
 
-func (b *BadgerAdapter) DeletePackageFileEntry(packageName string) error {
+func (b *BadgerAdapter) DeletePackageFileEntry(packageName, packageArch string) error {
 	// Convert to bytes outside the txn to reduce time spent in txn.
 	key := []byte(packageName)
 
